074f6b32e5780f46dc7246580b4a95c69945a1f9
Your Name <you@example.com>
2021-09-07 18:55:22 +0700

    delete client-side cookie on logout with react-cookie

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -4,15 +4,19 @@ import { getDataFromTree } from "@apollo/client/react/ssr";
 import { useLogoutMutation, useMeQuery } from "../src/generated/operation";
 import { useRouter } from "next/router";
 import { ME_QUERY } from "../src/graphql/queries";
+import { useCookies } from "react-cookie";
 
 const Logout = () => {
-    const [logout] = useLogoutMutation({refetchQueries: [{query: ME_QUERY}]});
+    const [cookies, setCookies, removeCookie] = useCookies(["accessToken"]);
+
+    const [logout] = useLogoutMutation({refetchQueries: [{ query: ME_QUERY }]});
 
     const router = useRouter();
 
     useEffect(() => {
         logout().then(() => {
-            router.push("/")
+            removeCookie('accessToken');
+            router.push("/");
         });
     }, []);
 
